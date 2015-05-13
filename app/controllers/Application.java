db32b8d589542710c50b4067275eef32915b0ae8
User <User@H27-NotePC07>
2015-05-13 14:24:34 +0900

    フォームの追加 まだあいまいだから マージはだめ

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 import models.Task;
 import java.util.Date;
 import java.util.Random;
+import java.util.Map;
 
 public class Application extends Controller {
 
@@ -38,6 +39,17 @@ public class Application extends Controller {
     public static Result help(){
 
     	return ok(help.render());
+    }
+
+
+    public static Result createTask() {
+        // java.util.Map をインポートしておく
+        Map<String, String[]> params = request().body().asFormUrlEncoded();
+
+        Task newTask = new Task();
+        newTask.name = params.get("name")[0]; // <input type="text" name="name" /> に入力された値
+        newTask.save();
+        return redirect(routes.Application.tasks());
     }
 
 
